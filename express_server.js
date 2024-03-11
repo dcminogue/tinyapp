bc01363c625789c47d208c7cb9ebc64406b622b3
Daniel Minogue <danielcminogue@gmail.com>
2024-03-10 21:43:05 -0600

    Add GET route to express_server.js and render using accompanying template. Add POST route to express_server.js to receive form submission.

--- express_server.js
+++ express_server.js
@@ -4,14 +4,30 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString(length) {
+    const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+        );
+    }
+    return result;
+}
+
+console.log(generateRandomString(6));
+
 const urlDatabase = {
     b2xVn2: "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com",
 };
 
-app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -20,6 +36,16 @@ app.get("/urls/:id", (req, res) => {
         longURL: urlDatabase[req.params.id],
     };
     res.render("urls_show", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 // app.get("/", (req, res) => {
