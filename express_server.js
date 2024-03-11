9bba6f7fbc91192fbcba513339bb0284bd367145
Daniel Minogue <danielcminogue@gmail.com>
2024-03-11 13:35:36 -0600

    Update express_server.js to store short-url and long-url key value pair to database when a POST request is received to /urls. Update express_server.js to respond with a redirect from /urls to /urls/:id.

--- express_server.js
+++ express_server.js
@@ -19,8 +19,6 @@ function generateRandomString(length) {
     return result;
 }
 
-console.log(generateRandomString(6));
-
 const urlDatabase = {
     b2xVn2: "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com",
@@ -43,9 +41,17 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+app.get("/u/:id", (req, res) => {
+    const longURL = urlDatabase[req.params.id];
+    res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
-    console.log(req.body); // Log the POST request body to the console
-    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+    const id = generateRandomString(6);
+    const url = req.body.longURL;
+    urlDatabase[id] = url;
+    // urlDatabase.id = url; // Log the POST request body to the console
+    res.redirect(`/urls/${id}`); // Respond with 'Ok' (we will replace this)
 });
 
 // app.get("/", (req, res) => {
